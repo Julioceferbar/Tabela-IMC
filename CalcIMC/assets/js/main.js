716c16dc5129886a8d515e0ff128d09e5de83802
Julio César <julioceferbar@gmail.com>
2021-08-05 00:23:57 -0300

    atualizando 1

--- CalcIMC/assets/js/main.js
+++ CalcIMC/assets/js/main.js
@@ -1,32 +1,66 @@
 //capturar evento de submit do formulario
 const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', function (e) { //evento
-    e.preventDefault();
-    const inputPeso = e.target.querySelector('#peso'); 
-    const inputAltura = e.target.querySelector('#altura'); //exatamente qual elemento foi acionado na tela
-    const peso = Number(inputPeso.value);
-    const altura1 = Number(inputAltura.value);
+form.addEventListener('submit', function (event) {
+  //evento
+  event.preventDefault();
+  const inputPeso = e.target.querySelector('#peso');
+  const inputAltura = e.target.querySelector('#altura'); //exatamente qual elemento foi acionado na tela
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 
-    if (!peso) {
-        setResultado('Peso invalido');
-        return; 
+  if (!peso) {
+    setResultado('Peso invalido', false);
+    return;
+  } 
 
-    }
-    // continua o codigo    
+  if (!altura) {
+    setResultado('Altura invalida', false);
+    return;
+  }
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc);
+
+  const msg = `Seu IMC é ${imc} (${nivelImc}.)`;
+
+  setResultado(msg, true);
+  // continua o codigo...
 });
-
-function criaP () {
-    const p = document.createElement('p');
-    return p;
-}
-
-
-function setResultado (msg) { //colocar um html dentro da div resultado
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
     
-    const p = criaP();
-
+    if (imc >= 39.9) return nivel[5]
+    if (imc >= 34.9) return nivel[4]
+    if (imc >= 29.9) return nivel[3]
+    if (imc >= 24.9) return nivel[2]
+    if (imc >= 18.5) return nivel[1]
+    if (imc < 18.5) return nivel[0]
 }
-fffff
+
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2)
+}
+// criar paraglafo
+
+function criaP() {
+  const p = document.createElement('p');
+  return p;
+}
+
+function setResultado (msg, isValid) {
+  //colocar um html dentro da div resultado
+  const resultado = document.querySelector('#resultado');
+  resultado.innerHTML = '';
+  
+  const p = criaP();
+
+  if (isValid) {
+    p.classList.add ('paraglafo-resultado');
+  } else {
+      p.classList.add('bad');
+  }  
+
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
